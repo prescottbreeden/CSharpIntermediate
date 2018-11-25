7bd38aef2c1de6bfe87247737f836f885715dd4b
prescottbreeden <prescott.breeden@gmail.com>
2018-11-25 02:04:29 -0800

    Person class added

---
+++ CSharpIntermediate/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpIntermediate
+{
+    public class Person
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Introduce()
+        {
+            System.Console.WriteLine("Hi, my name is " + Name);
+        }
+
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -7,6 +7,20 @@ namespace CSharpIntermediate
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Anatomy of a class: Data and Methods
+            // Class Name
+            // Data Names and types
+            // Method Names
+
+            // e.g.
+            // Post
+            // Title: string
+            // Publish()
+
+            // Object is an instance of a class that resides in memory
+
+            Person bob = new Person("bob", 36);
+            bob.Introduce();
         }
     }
 }
